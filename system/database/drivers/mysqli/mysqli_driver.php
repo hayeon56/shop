18e565c4ccd7b3c0b719ee1b1c2be8e82a749492
nekoten <souraru1@gmail.com>
2019-07-03 16:16:27 +0900

    sully

--- system/database/drivers/mysqli/mysqli_driver.php
+++ system/database/drivers/mysqli/mysqli_driver.php
@@ -132,7 +132,7 @@ class CI_DB_mysqli_driver extends CI_DB {
 		}
 
 		$client_flags = ($this->compress === TRUE) ? MYSQLI_CLIENT_COMPRESS : 0;
-		$this->_mysqli = mysqli_init();
+	//	$this->_mysqli = mysqli_init();
 
 		$this->_mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 10);
 
